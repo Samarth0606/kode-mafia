34873adefa6f790361c53b2e49fc3846500f18fc
Samarth <officialdevelopersamarth@gmail.com>
2024-07-21 17:37:27 +0530

    lecture 43 codes-> ts

---
+++ Lecture-43/ts-cont/src/a.ts
@@ -0,0 +1,93 @@
+
+// let arr:number[] = [1,32,3,4,4,5];
+
+
+// let arr2:number[] |  string[] = ["1","2"];
+
+
+// ---------------------------------------
+
+
+
+// let arro = ()=>{
+//     return 190
+// }
+// console.log(arro())
+
+
+// ------------------------
+
+// function sam(a:number,b:number){
+//     console.log(a+b)
+// }
+// sam(10,20)
+
+
+// --------------------------
+
+// enum -> keyword -> used for creating user defined constant value -> iterateb
+
+//snake game
+
+// keypressed -> up , down , left , right
+
+
+// type Key = "up" | "down" | "right" | "left";
+// enum Key{
+//     up = "up"  , 
+//     right = "right" , 
+//     left ="left" , 
+//     down ="down"
+// }
+
+// function dabadia(keypressed:Key){
+//     if(keypressed === Key.up){
+//         console.log("up")
+//     }
+//     else if(keypressed === Key.down){
+//         console.log("down")
+//     }
+// }
+// console.log(Key.up)
+// console.log(Key.right)
+// console.log(Key.left)
+// console.log(Key.down)
+// dabadia(Key.up)
+// dabadia(Key.down)
+// dabadia(Key.chandigarh)
+
+
+// --------------------------------
+// enum Responses{
+//     Success = 201,
+//     Notfound = 404,
+//     Error = 500
+// }
+
+// app.get('/basiuri' , (req,res)=>{
+//     res.status(Responses.Success).send("hello")
+// })
+
+// app.get('/basiuri' , (req,res)=>{
+//     res.status(Responses.Success).send("hello")
+// })
+
+// app.get('/basiuri' , (req,res)=>{
+//     res.status(Responses.Success).send("hello")
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
